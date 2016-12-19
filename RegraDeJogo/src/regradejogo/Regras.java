8a00e6c3069fc5e671b069c275ce1a991335ebdc
Max Fratane <mfratane@id.uff.br>
2016-12-19 00:55:21 -0200

    BugFix.
    fix #23

--- RegraDeJogo/src/regradejogo/Regras.java
+++ RegraDeJogo/src/regradejogo/Regras.java
@@ -108,6 +108,8 @@ public class Regras {
                 if(!possuiCaptura(capturasPecaAtual)){
                     trocaJogadorAtual();
                 }
+            }else{
+                trocaJogadorAtual();
             }
 
         } else {
